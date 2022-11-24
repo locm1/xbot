f81a8d63c87e7bd7e96633c086afb11fbb3f0a0e
s-fukushi <fukushi@reno-m.jp>
2022-11-24 10:55:19 +0900

    vite error

---
+++ resources/js/index.jsx
@@ -0,0 +1,36 @@
+
+// =========================================================
+// * Volt Pro React Dashboard
+// =========================================================
+
+// * Product Page: https://themesberg.com/product/dashboard/volt-pro-react
+// * Copyright 2021 Themesberg (https://www.themesberg.com)
+// * License Information (https://themesberg.com/licensing)
+
+// * Designed and coded by https://themesberg.com
+
+// =========================================================
+
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
+
+// core styles
+// import "./../scss/volt.scss";
+
+// vendor styles
+//import "leaflet/dist/leaflet.css";
+//import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+// import "react-datetime/css/react-datetime.css";
+
+// import HomePage from "./pages/HomePage";
+// import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);

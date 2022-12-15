c6555b900ed86b3076ec80c2ae866f9b8af183cb
s-fukushi <fukushi@reno-m.jp>
2022-12-15 18:49:52 +0900

    ViteからLaravelmixに変更

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,22 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 mix.js('resources/js/index.jsx', 'public/js')
-.react();
+.react()
+.alias({
+  '@':path.join(__dirname, 'resources/js'),
+  '@img':path.join(__dirname, 'resources/assets'),
+  '@scss':path.join(__dirname, '/resources/scss')
+});
+
+
+
+// mix.webpackConfig({
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, '/resources/js'),
+//       '@img': path.resolve(__dirname, '/resources/assets'),
+//       '@scss': path.resolve(__dirname, '/resources/scss'),
+//     }
+//   }
+// });

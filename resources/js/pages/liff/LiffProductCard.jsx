23d7c7e324066963b57283cae8a5ae3b3a030ede
s-fukushi <fukushi@reno-m.jp>
2023-01-12 12:11:20 +0900

    LIFF一覧画面

--- resources/js/pages/liff/LiffProductCard.jsx
+++ resources/js/pages/liff/LiffProductCard.jsx
@@ -11,7 +11,7 @@ export default (props) => {
     <Card border="0" className="shadow p-0">
       <Link to={`/liff/product/detail/${id}`}>
         <div style={{ backgroundImage: `url(${img})` }} className="profile-cover rounded-top liff-product-card-img" />
-        <Card.Body className="pb-5 rounded-bottom">
+        <Card.Body className="pb-3 rounded-bottom p-2 pt-3">
           <Card.Title className="liff-product-card-title">{name}</Card.Title>
           <Card.Subtitle className="fw-bold liff-product-card-price">{price}</Card.Subtitle>
         </Card.Body>

ede93c5aa494b4e4b495b0b2a18533efd3af3052
sho suematsu <43801777+suesho1020@users.noreply.github.com>
2023-01-23 13:06:13 +0900

    利用規約の処理を追加

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\PrivacyPolicyController;
+use App\Http\Controllers\api\TermsOfServiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/setting/privacy-policy', [PrivacyPolicyController::class, 'index']);
 Route::post('/setting/privacy-policy/store', [PrivacyPolicyController::class, 'store']);
 Route::post('/setting/privacy-policy/update', [PrivacyPolicyController::class, 'update']);
+
+Route::get('/setting/terms-of-service', [TermsOfServiceController::class, 'index']);
+Route::post('/setting/terms-of-service/store', [TermsOfServiceController::class, 'store']);
+Route::post('/setting/terms-of-service/update', [TermsOfServiceController::class, 'update']);

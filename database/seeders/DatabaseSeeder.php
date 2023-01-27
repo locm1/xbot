33ba7dfaa9b9ed2bfea87d04cc0c30b2b11b06ef
locm1 <ak.47_teytey@icloud.com>
2023-01-27 13:24:28 +0900

    ログイン時にFormを使用するよう変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,5 @@ class DatabaseSeeder extends Seeder
             AdminTableSeeder::class
         ]);
         \App\Models\User::factory(100)->create();
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }

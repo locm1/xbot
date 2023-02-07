856b71b6745ea4e0f5e76ab7e15c1ed05f708450
locm1 <ak.47_teytey@icloud.com>
2023-02-07 16:17:50 +0900

    商品管理フロント完成

--- resources/js/pages/product/EditProduct.jsx
+++ resources/js/pages/product/EditProduct.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import moment from "moment-timezone";
 import { useDropzone } from "react-dropzone";
-import { CalendarIcon, XIcon, HomeIcon, PlusIcon, SearchIcon, CogIcon, QuestionMarkCircleIcon } from "@heroicons/react/solid";
-import { Col, Row, Form, Card, Image, Breadcrumb, Button, Dropdown, InputGroup, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { CalendarIcon, XIcon, HomeIcon, PlusIcon, SearchIcon, TrashIcon, QuestionMarkCircleIcon } from "@heroicons/react/solid";
+import { Col, Row, Form, Card, Image, Breadcrumb, Button, Dropdown, InputGroup, Tab, Nav } from 'react-bootstrap';
 import CheckboxButton from "@/components/CheckboxButton";
 import { Link, useHistory } from 'react-router-dom';
 import { Paths } from "@/paths";
 
 import ProductOverview from "@/pages/product/ProductOverview";
 import products from "@/data/products";
+import Select from 'react-select'
 
 export default () => {
   const [transactions, setTransactions] = useState(products.map(t => ({ ...t, show: true })));
@@ -24,13 +25,16 @@ export default () => {
     {id: 5, name: '食品'},
   ]
 
-  const relateProducts = [
-    {id: 1, name: '白菜'},
-    {id: 2, name: 'カレー'},
-    {id: 3, name: 'ロボット'},
-    {id: 4, name: '宇宙'},
-    {id: 5, name: '海'},
-  ]
+  const [relatedProducts, setRelatedProducts] = useState([
+    {id: 1, name: 'シャンプー&トリートメント', discountPrice: 200},
+    {id: 2, name: '北海道ミルクプリン', discountPrice: 200},
+    {id: 3, name: '生搾りジュース', discountPrice: 200},
+    {id: 4, name: '宇宙', discountPrice: 200},
+    {id: 5, name: '海', discountPrice: 200},
+  ]);
+  if (!relatedProducts.some(v => v.id === 0)) {
+    relatedProducts.push({id: 0, name: '', discountPrice: 0})
+  }
 
   const [files, setFiles] = useState([]);
   const { getRootProps, getInputProps } = useDropzone({
@@ -99,6 +103,50 @@ export default () => {
 
   const [privateProduct, setPrivate] = useState(false);
 
+  // セット割商品の選択オプションを登録商品から出すが、すでに選択されているものは除外する
+  const options = products.filter(v => !(relatedProducts.some(b => b.name === v.name))).map(v => ({value: v.name, label: v.name }))
+
+  const handleRelatedProducts = (e, id) => {
+    if (id === 0) {
+      // const maxId = Math.max({...relatedProducts}.id);
+      const ids = relatedProducts.map(v => (v.id));
+      const maxId = Math.max(...ids)
+      const newRelatedProducts = relatedProducts.map(v => {
+        if (v.id === 0) {
+         return {id: maxId + 1, name: e.value, price: 0}
+        } else {
+          return {...v}
+        }
+      })
+      setRelatedProducts(newRelatedProducts);
+    } else {
+      const newRelatedProducts = relatedProducts.map(v => {
+        if (v.id === id) {
+          return {id: maxId + 1, name: e.value, price: v.price}
+        } else {
+          return {...v}
+        }
+      });
+      setRelatedProducts(newRelatedProducts);
+    }
+  }
+
+  const deleteRelatedProducts = (id) => {
+    const newRelatedProducts = relatedProducts.filter(v => (v.id !== id));
+    setRelatedProducts(newRelatedProducts);
+  }
+
+  const handleRelatedPrice = (e, id) => {
+    const newRelatedProducts = relatedProducts.map(v => {
+      if (v.id === id) {
+        return {id: v.id, name: v.name, price: e.target.value}
+      } else {
+        return {...v}
+      }
+    });
+    setRelatedProducts(newRelatedProducts);
+  }
+
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
@@ -106,116 +154,146 @@ export default () => {
           <h1 className="page-title">商品編集</h1>
         </div>
       </div>
-      <Row>
-        <Col xs={12} xl={6}>
-          <Card border="0" className="shadow mb-4">
-            <Card.Body>
-              <div className="d-flex justify-content-between">
-              <h5 className="mb-4 border-bottom pb-3">商品情報</h5>
-              <Form.Group id="category">
-                <Form.Check
-                type="switch"
-                label="非公開にする"
-                id="switch1"
-                htmlFor="switch1"
-                onClick={() => setPrivate(!privateProduct)}
-                />
-              </Form.Group>
-              </div>
-              <Form>
-                <Col xs={12} xl={12}>
+      <Tab.Container defaultActiveKey="products" className="mb-6">
+        <Nav fill variant="pills" className="flex-column flex-sm-row">
+          <Nav.Item>
+            <Nav.Link eventKey="products" className="mb-sm-3 mb-md-0">
+              商品情報
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="related_products" className="mb-sm-3 mb-md-0">
+              セット割商品
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          <Tab.Pane eventKey="products" className="py-4">
+            <Col xs={12} xl={12}>
+              <Card border="0" className="shadow mb-4">
+                <Card.Body>
+                  <div className="d-flex justify-content-between">
+                  <h5 className="mb-4 border-bottom pb-3">商品情報</h5>
+                  <Form.Group id="category">
+                    <Form.Check
+                    type="switch"
+                    label="非公開にする"
+                    id="switch1"
+                    htmlFor="switch1"
+                    onClick={() => setPrivate(!privateProduct)}
+                    />
+                  </Form.Group>
+                  </div>
+                    <Col xs={12} xl={12}>
+                      <Row>
+                        <Col md={12} className="mb-3">
+                          <Form.Group id="name">
+                            <Form.Label>商品名</Form.Label>
+                            <Form.Control required type="text" name="name" placeholder="" />
+                          </Form.Group>
+                        </Col>
+                        <Col md={6} className="mb-3">
+                          <Form.Group id="name">
+                            <Form.Label>税率</Form.Label>
+                            <InputGroup>
+                              <Form.Control required type="number" name="tax" placeholder="" />
+                              <InputGroup.Text>％</InputGroup.Text>
+                            </InputGroup>
+                          </Form.Group>
+                        </Col>
+                        <Col md={6} className="mb-3">
+                          <Form.Group id="price">
+                            <Form.Label>販売価格（税込）</Form.Label>
+                            <InputGroup>
+                              <Form.Control required type="number" name="price" placeholder="" />
+                              <InputGroup.Text>円</InputGroup.Text>
+                            </InputGroup>
+                          </Form.Group>
+                        </Col>
+                        <Col md={6} className="mb-3">
+                          <Form.Label>在庫数</Form.Label>
+                          <InputGroup className="me-2 me-lg-3">
+                            <InputGroup.Text className="d-flex">
+                              <Form.Check id="checkbox1" htmlFor="checkbox1" onClick={() => setDisable(!disableInputForm)} />
+                              <Form.Label htmlFor="checkbox1" className="mb-0">無制限</Form.Label>
+                            </InputGroup.Text>
+                            <Form.Control
+                              disabled={disableInputForm}
+                              type="number"
+                              placeholder=""
+                              id="stock_quantity"
+                              name="stock_quantity"
+                            />
+                          </InputGroup>
+                        </Col>
+                        <Col md={6} className="mb-3">
+                          <Form.Group id="category">
+                            <Form.Label>カテゴリー</Form.Label>
+                            <Form.Select defaultValue="0" className="mb-0">
+                              {
+                                categories.map((category, index) => <option key={index} value={category.id}>{category.name}</option>)
+                              }
+                            </Form.Select>
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                      <Col md={12} className="mb-3">
+                        <Form.Group id="overview">
+                          <Form.Label>商品概要</Form.Label>
+                          <Form.Control as="textarea" rows="3" />
+                        </Form.Group>
+                      </Col>
+                          <Form.Label>商品画像</Form.Label>
+                      <Form {...getRootProps({ className: "dropzone rounded d-flex align-items-center justify-content-center mb-4" })}>
+                        <Form.Control {...getInputProps()} />
+                        <div className="dz-default dz-message text-center">
+                          <p className="dz-button mb-0">画像をアップロード、もしくはドラッグアンドドロップをしてください。</p>
+                        </div>
+                      </Form>
+                      <Row className="dropzone-files">
+                        {files.map(file => <DropzoneFile key={file.path} {...file} />)}
+                      </Row>
+                    </Col>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Tab.Pane>
+        </Tab.Content>
+        <Tab.Content>
+          <Tab.Pane eventKey="related_products" className="py-4 pb-12">
+            <Col xs={12} xl={12}>
+              <Card border="0" className="shadow mb-4">
+                <Card.Body>
+                  <h5 className="mb-4 border-bottom pb-3">セット割商品</h5>
                   <Row>
-                    <Col md={12} className="mb-3">
-                      <Form.Group id="name">
-                        <Form.Label>商品名</Form.Label>
-                        <Form.Control required type="text" name="name" placeholder="" />
-                      </Form.Group>
-                    </Col>
-                    <Col md={6} className="mb-3">
-                      <Form.Group id="name">
-                        <Form.Label>税率</Form.Label>
-                        <Form.Control required type="number" name="tax" placeholder="" />
-                      </Form.Group>
-                    </Col>
-                    <Col md={6} className="mb-3">
-                      <Form.Group id="price">
-                        <Form.Label>販売価格（税込）</Form.Label>
-                        <Form.Control required type="number" name="price" placeholder="" />
-                      </Form.Group>
-                    </Col>
-                    <Col md={6} className="mb-3">
-                      <Form.Label>在庫数</Form.Label>
-                      <InputGroup className="me-2 me-lg-3 fmxw-300">
-                        <InputGroup.Text className="d-flex">
-                          {/* <Form.Label htmlFor="checkbox1">無制限</Form.Label> */}
-                          <OverlayTrigger
-                          key="example"
-                          overlay={<Tooltip id="top" className="m-0">在庫数を無制限とします</Tooltip>}
-                          >
-                            {/* <QuestionMarkCircleIcon className="icon icon-xs me-1" /> */}
-                          <Form.Check id="checkbox1" htmlFor="checkbox1" onClick={() => setDisable(!disableInputForm)} />
-                          </OverlayTrigger>
-                        </InputGroup.Text>
-                        <Form.Control
-                          disabled={disableInputForm}
-                          type="number"
-                          placeholder=""
-                          id="stock_quantity"
-                          name="stock_quantity"
-                        />
-                      </InputGroup>
-                    </Col>
-                    <Col md={6} className="mb-3">
-                      <Form.Group id="category">
-                        <Form.Label>カテゴリー</Form.Label>
-                        <Form.Select defaultValue="0" className="mb-0">
-                          {
-                            categories.map((category, index) => <option key={index} value={category.id}>{category.name}</option>)
-                          }
-                        </Form.Select>
-                      </Form.Group>
-                    </Col>
+                    {relatedProducts.map((v,k) => (
+                      <React.Fragment key={`relatedProducts-${k}`}>
+                        <Col xl={8} className={"my-2"}>
+                          <Select options={options} value={{label: v.name, value: v.name}} onChange={(e) => handleRelatedProducts(e, v.id)} />
+                        </Col>
+                        <Col xl={3} className={"my-2"}>
+                        <Form.Group>
+                          <InputGroup>
+                            <Form.Control required type="number" name="tax" placeholder="" disabled={v.name ? false : true} value={v.discountPrice} onChange={(e) => handleRelatedPrice(e, v.id)} />
+                            <InputGroup.Text>円OFF</InputGroup.Text>
+                          </InputGroup>
+                        </Form.Group>
+                        </Col>
+                        <Col xl={1}>
+                          <div className="d-flex h-100 align-items-center">
+                            <TrashIcon onClick={v.id ? () => deleteRelatedProducts(v.id) : null} role={`button`} className={`icon icon-xs ${v.id ? "text-danger" : "text-bg-gray not-allowed"}`} />
+                          </div>
+                        </Col>
+                      </React.Fragment>
+                    ))}
                   </Row>
-                  <Col md={12} className="mb-3">
-                    <Form.Group id="overview">
-                      <Form.Label>商品概要</Form.Label>
-                      <Form.Control as="textarea" rows="3" />
-                    </Form.Group>
-                  </Col>
-                </Col>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} xl={6}>
-          <Card border="0" className="shadow mb-4">
-            <Card.Body>
-              <h5 className="mb-4 border-bottom pb-3">商品画像</h5>
-              <Form {...getRootProps({ className: "dropzone rounded d-flex align-items-center justify-content-center mb-4" })}>
-                <Form.Control {...getInputProps()} />
-                <div className="dz-default dz-message text-center">
-                  <p className="dz-button mb-0">画像をアップロード、もしくはドラッグアンドドロップをしてください。</p>
-                </div>
-              </Form>
-              <Row className="dropzone-files">
-                {files.map(file => <DropzoneFile key={file.path} {...file} />)}
-              </Row>
-            </Card.Body>
-          </Card>
-          <Card border="0" className="shadow mb-4">
-            <Card.Body>
-              <h5 className="mb-4 border-bottom pb-3">抱き合わせ商品選択</h5>
-              <Form.Select>
-                <option defaultValue>選択</option>
-                {relateProducts.map(product => <option key={product.id}>{product.name}</option>)}
-              </Form.Select>
-              <Row className="dropzone-files">
-                {files.map(file => <DropzoneFile key={file.path} {...file} />)}
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} xl={12}>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
+        {/* <Col xs={12} xl={12}>
           <Card border="0" className="shadow mb-4">
             <Card.Body>
               <h5 className="mb-4 border-bottom pb-3">商品概要</h5>
@@ -230,13 +308,7 @@ export default () => {
               </Button>
             </Card.Body>
           </Card>
-        </Col>
-      </Row>
-      <div className="d-flex justify-content-end m-4 text-decoration-underline">
-        <Link to={Paths.Display.path}>
-          商品ディスプレイ変更
-        </Link>
-      </div>
+        </Col> */}
     </>
   );
 };

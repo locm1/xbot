fd4eeec851bdece9c3bcab6a848a7b28ca410272
locm1 <ak.47_teytey@icloud.com>
2023-03-16 17:27:04 +0900

    リッチメニュー途中

--- app/Services/management/rich_menu/RichMenuService.php
+++ app/Services/management/rich_menu/RichMenuService.php
@@ -10,12 +10,17 @@ use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBuilder;
 use LINE\LINEBot\RichMenuBuilder\RichMenuSizeBuilder;
 use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
 use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use App\Services\api\LineBotService as LINEBot;
+use App\Services\management\rich_menu_ailias\RichMenuAiliasService;
 
 class RichMenuService
 {
     protected $bot;
 
-    public function __construct($bot) {
+    public function __construct() {
+        $httpClient = new CurlHTTPClient(config('services.line.message.channel_token'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => config('services.line.message.channel_secret')]);
         $this->bot = $bot;
     }
 
@@ -23,40 +28,85 @@ class RichMenuService
     {
         $response = $this->bot->getRichMenuList()->getJSONDecodedBody();
         foreach ($response['richmenus'] as $richmenu) {
-            $data[] = ['richMenuId' => $richmenu['richMenuId'], 'name' => $richmenu['name']];
+            $form_value[] = ['richMenuId' => $richmenu['richMenuId'], 'name' => $richmenu['name']];
         }
-        return $data;
+        return $form_value;
     }
 
 
     public function store($request) 
     {
-        $data = $request->all();
+        $form_value = $request->all();
         $is_default = true;
 
         $height = 810;
         $width = 1200;
         $richMenuSizeBuilder = new RichMenuSizeBuilder($height, $width);
-        $action = new RichMenuAction($data);
+        $action = new RichMenuFormatAction($form_value);
 
         $create_menu_area = new CreateRichMenuTemplateAction($action->getTemplateType(), $action->getActionTypes(), $action->getActions());
-        $richMenuBuilder = new RichMenuBuilder($richMenuSizeBuilder, $is_default, $data['title'], $data['menuBarText'], $create_menu_area->create());
+        $richMenuBuilder = new RichMenuBuilder($richMenuSizeBuilder, $is_default, $form_value['title'], $form_value['menuBarText'], $create_menu_area->create());
         $create_richmenu_response = $this->bot->createRichMenu($richMenuBuilder);
 
         //リッチメニューの画像をアップロードする
         $richmenu_id = $create_richmenu_response->getJSONDecodedBody()['richMenuId'];
-        // Log::channel('line_webhook')->info(print_r($richmenu_id, true));
         $image_path = $request->file('image')->path();
         $contentType = 'image/png';
         $response = $this->bot->uploadRichMenuImage($richmenu_id, $image_path, $contentType);
 
-        return $response->getJSONDecodedBody();
+        $reduce_richmenu_id = str_replace('richmenu-', '', $richmenu_id);
+        $ailias_service = new RichMenuAiliasService;
+        $response = $ailias_service->store($reduce_richmenu_id, $richmenu_id);
+
+        //デフォルトのリッチメニューに登録する
+        $this->bot->setDefaultRichMenuId($richmenu_id);
+
+        return $richmenu_id;
     }
 
 
     public function show($id) 
     {
-        return $this->bot->getRichMenu($id)->getJSONDecodedBody();
+        $line_data = $this->bot->getRichMenu($id)->getJSONDecodedBody();
+        $form_value = [
+            'title' => $line_data['name'],
+            'menuBarText' => $line_data['chatBarText'],
+        ];
+        $action = new RichMenuAction;
+        $form_value['menuType'] = $action->getTemplateType($line_data['size'], $line_data['areas']);
+        foreach ($line_data['areas'] as $k => $v) {
+            $actions = new RichMenuConvertAction($v);
+            switch ($v['action']['label']) {
+                case 'a':
+                    $form_value['A-type'] = $actions->getActionType();
+                    $form_value['A-value'] = $actions->getValue();
+                    break;
+                case 'b':
+                    $form_value['B-type'] = $actions->getActionType();
+                    $form_value['B-value'] = $actions->getValue();
+                    break;
+                case 'c':
+                    $form_value['C-type'] = $actions->getActionType();
+                    $form_value['C-value'] = $actions->getValue();
+                    break;
+                case 'd':
+                    $form_value['D-type'] = $actions->getActionType();
+                    $form_value['D-value'] = $actions->getValue();
+                    break;
+                case 'e':
+                    $form_value['E-type'] = $actions->getActionType();
+                    $form_value['E-value'] = $actions->getValue();
+                    break;
+                case 'f':
+                    $form_value['F-type'] = $actions->getActionType();
+                    $form_value['F-value'] = $actions->getValue();
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+        }
+        return $form_value;
     }
 
 
@@ -66,9 +116,21 @@ class RichMenuService
     }
 
 
-    public function destroy() 
+    public function destroy($id) 
     {
-        //
+        //リッチメニューエイリアスを削除
+        $data = $this->bot->getRichMenuAliasList()->getJSONDecodedBody()['aliases'];
+        foreach ($data as $k => $v) {
+            if ($v['richMenuId'] === $id) {
+                $alias_id = $v['richMenuAliasId'];
+            }
+        }
+        if ($alias_id ?? false) {
+            $this->bot->deleteRichMenuAlias($alias_id)->getJSONDecodedBody();
+        }
+
+        //リッチメニューを削除
+        return $this->bot->deleteRichMenu($id)->getJSONDecodedBody();
     }
 
 }

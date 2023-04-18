99840be547daf164095f55bc74b5d59b9759eb51
locm1 <ak.47_teytey@icloud.com>
2023-04-18 13:53:40 +0900

    招待管理中途半端分

--- app/Http/Controllers/LineWebhookController.php
+++ app/Http/Controllers/LineWebhookController.php
@@ -8,6 +8,7 @@ use App\Services\api\line\invite\InviteService;
 use App\Services\api\line\UnfollowService;
 use Illuminate\Http\Request;
 use App\Services\api\LineBotService as LINEBot;
+use App\Services\management\invitation\InviteeIncentiveService;
 use App\Services\liff\invite\InviteIncentiveJobService;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use Illuminate\Support\Facades\Log;
@@ -33,16 +34,20 @@ class LineWebhookController extends Controller
             if ($event['type'] === 'follow') {
                 $follow_service = new FollowService($bot, $event['source']['userId']);
                 $invite_incentive_job_service = new InviteIncentiveJobService;
+                $invitee_incentive_service = new InviteeIncentiveService;
+                //ユーザー作成
                 $User = $follow_service->createUser();
+                //流入経路
                 $update_count = $follow_service->checkInflowRoute($User);
+                //招待管理
                 $InviteIncentiveJob = $invite_incentive_job_service->searchByLineId($User->line_id);
                 if ($InviteIncentiveJob) $InviteIncentiveJob->update(['invitee_user_id' => $User->id]);
-
+                //招待クーポン発行
 
                 $greeting_service = new GreetingService($bot, $event['source']['userId'], $event['replyToken']);
-                $invite_service = new InviteService($bot, $User);
-                $greeting_service->sendGreetingMessage();
-                return $invite_service->sendInviteMessage($invitee_incentive_user->is_issued, 'invitee', 1);
+                // $invite_service = new InviteService($bot, $User);
+                return $greeting_service->sendGreetingMessage();
+                // return $invite_service->sendInviteMessage($invitee_incentive_user->is_issued, 'invitee', 1);
                 
             } elseif ($event['type'] === 'unfollow') {
                 $unfollow_service = new UnfollowService($bot, $event['source']['userId']);

c41c8b8bfec1c2f326f52fcee047c783c73923f9
s-fukushi <fukushi@reno-m.jp>
2023-04-26 17:14:52 +0900

    編集後、権限が「管理者」のままになる 修正

--- resources/js/pages/account/api/AdminApiMethods.jsx
+++ resources/js/pages/account/api/AdminApiMethods.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 export const getAccounts = async (params, setAccounts, setLinks, setPaginate) => {
   axios.get('/api/v1/management/admins', params)
   .then((response) => {
@@ -16,3 +18,46 @@ export const getAccounts = async (params, setAccounts, setLinks, setPaginate) =>
       console.error(error);
   });
 };
+
+export const getAccount = async (id, setFormValue) => {
+  await axios.get(`/api/v1/management/admins/${id}`)
+  .then((response) => {
+    const admin = response.data.admin;
+    setFormValue({...admin, password: '', password_confirmation: ''})
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+export const storeAccount = async (formValue, setError) => {
+  await axios.post('/api/v1/management/admins', formValue)
+  .then((response) => {
+    Swal.fire(
+      '登録完了',
+      'アカウントの登録に成功しました',
+      'success'
+    )
+    console.log(response);
+  })
+  .catch(error => {
+    console.error(error);
+    setError(error.response.data.errors)
+  });
+}
+
+export const updateAccount = async (id, formValue, setError) => {
+  await axios.put(`/api/v1/management/admins/${id}`, formValue)
+  .then((response) => {
+    Swal.fire(
+      '更新完了',
+      'アカウントの更新に成功しました',
+      'success'
+    )
+    console.log(response);
+  })
+  .catch(error => {
+    console.error(error);
+    setError(error.response.data.errors)
+  });
+}

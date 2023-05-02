cd5182ecef0ebe5cf3ab3c666e3c1c64603f4089
locm1 <ak.47_teytey@icloud.com>
2023-05-02 19:39:15 +0900

    保存ボタン

--- resources/js/pages/questionnaire/DefaultQuestionnaire.jsx
+++ resources/js/pages/questionnaire/DefaultQuestionnaire.jsx
@@ -34,16 +34,16 @@ export default () => {
     }
     updateUserInfoStatuses(formValue)
   };
-  
+
   const changeQuestionnaireEnabling = (isValid) => {
-    const updateData = {...questionnaireEnabling, ['is_default_questionnaire_enabled']: isValid ? 1 : 0}
+    const updateData = { ...questionnaireEnabling, ['is_default_questionnaire_enabled']: isValid ? 1 : 0 }
     setIsValid(isValid)
     setQuestionnaireEnabling(updateData)
     updateQuestionnaireEnabling(1, updateData)
   }
 
   const TableRow = (props) => {
-    const { id, is_undisclosed, is_required, name  } = props;
+    const { id, is_undisclosed, is_required, name } = props;
 
     return (
       <tr className="border-bottom product-table-tr">
@@ -51,7 +51,7 @@ export default () => {
           <span className="fw-normal">{name}</span>
         </td>
         <td>
-          <Form.Select value={is_undisclosed} className="mb-0" onChange={(e) => {handleChange(e, id, 'is_undisclosed')}}>
+          <Form.Select value={is_undisclosed} className="mb-0" onChange={(e) => { handleChange(e, id, 'is_undisclosed') }}>
             <option value="0">公開</option>
             <option value="1">非公開</option>
           </Form.Select>
@@ -60,7 +60,7 @@ export default () => {
           <Form.Select
             value={is_required}
             className="mb-0"
-            onChange={(e) => {handleChange(e, id, 'is_required')}}
+            onChange={(e) => { handleChange(e, id, 'is_required') }}
             disabled={is_undisclosed == 1 && true}
           >
             <option value="0">任意</option>
@@ -76,26 +76,26 @@ export default () => {
     showQuestionnaireEnabling(1, setQuestionnaireEnabling, setIsValid, 'is_default_questionnaire_enabled')
   }, []);
 
-	return (
-		<>
-			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+  return (
+    <>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div className="d-block mb-4 mb-md-0">
           <h1 className="page-title">固定アンケート管理</h1>
         </div>
         <div className="d-flex flex-row-reverse mt-3">
           <Form.Group id="default-questionnaire">
             <Form.Check
-            type="switch"
-            label="固定アンケートをONにする"
-            id="is-default-questionnaire-valid"
-            htmlFor="is-default-questionnaire-valid"
-            checked={isValid}
-            onClick={() => changeQuestionnaireEnabling(!isValid)}
+              type="switch"
+              label="固定アンケートをONにする"
+              id="is-default-questionnaire-valid"
+              htmlFor="is-default-questionnaire-valid"
+              checked={isValid}
+              onClick={() => changeQuestionnaireEnabling(!isValid)}
             />
           </Form.Group>
         </div>
       </div>
-			<Card border="0" className="table-wrapper table-responsive shadow mb-4">
+      <Card border="0" className="table-wrapper table-responsive shadow mb-4">
         <Table className="align-items-center">
           <thead className="bg-primary text-white">
             <tr>
@@ -105,15 +105,17 @@ export default () => {
             </tr>
           </thead>
           <tbody className="border-0">
-            { userInfoStatuses && userInfoStatuses.map((userInfoStatus, index) => <TableRow key={`user-info-status-${userInfoStatus.id}`} {...userInfoStatus} />)}
+            {userInfoStatuses && userInfoStatuses.map((userInfoStatus, index) => <TableRow key={`user-info-status-${userInfoStatus.id}`} {...userInfoStatus} />)}
           </tbody>
         </Table>
-        <div className="d-flex flex-row-reverse py-3">
-          <Button onClick={handleClick} variant="success" className="me-2">
-            保存する
-          </Button>
-        </div>
+        <Card.Footer>
+          <div className="d-flex justify-content-end">
+            <Button variant="success" className="btn-default-success" onClick={handleClick}>
+              保存する
+            </Button>
+          </div>
+        </Card.Footer>
       </Card>
-		</>
-	)
+    </>
+  )
 }

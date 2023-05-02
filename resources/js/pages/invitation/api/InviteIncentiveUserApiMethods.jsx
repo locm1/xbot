bcc058b410c75884228470588de37415aa1ae0aa
locm1 <ak.47_teytey@icloud.com>
2023-05-02 19:31:46 +0900

    データがない時バグる

--- resources/js/pages/invitation/api/InviteIncentiveUserApiMethods.jsx
+++ resources/js/pages/invitation/api/InviteIncentiveUserApiMethods.jsx
@@ -12,7 +12,8 @@ export const getInviterIncentiveUsers = async (id, setInviterIncentiveUsers) => 
 export const getInviteeIncentiveUsers = async (id, setInviteeIncentiveUsers) => {
   axios.get(`/api/v1/management/invite-incentives/${id}/invitee-users`)
   .then((response) => {
-    setInviteeIncentiveUsers(response.data.invitee_incentive_users);
+    console.log(response.data.invitee_incentive_users);
+    setInviteeIncentiveUsers(response.data.invitee_incentive_users ?? []);
 
   })
   .catch(error => {

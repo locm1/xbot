c63684a3faf293aba91ad3118a031168181e8435
s-fukushi <fukushi@reno-m.jp>
2023-05-22 18:00:05 +0900

    商品画像バグ修正

--- resources/js/pages/product/EditProduct.jsx
+++ resources/js/pages/product/EditProduct.jsx
@@ -83,10 +83,10 @@ export default () => {
   };
 
   const deleteImage = (e, id, display_id) => {
-    e.stopPropagation();
+    // e.stopPropagation();
     const deleteImage = productImages.find((image) => image.id === id)
+    id && setStoreProductImages(storeProductImages.filter(image => image.id !== id))
     setProductImages(productImages.filter(image => image.display_id !== display_id))
-    setStoreProductImages(storeProductImages.filter(image => image.id !== id))
     // 削除対象のstate から、idがない、つまりフロント側で追加した画像をバックエンドに送らないようにする
     if (typeof deleteImage !== "undefined") {
       setDeleteProductImages([...deleteProductImages, deleteImage]);
@@ -126,15 +126,10 @@ export default () => {
       }
       reader.readAsDataURL(e.target.files[0])
     };
-
-    const test = () => {
-      console.log('a');
-      
-    };
     
 
     return (
-      <div className="dropzone-preview py-2" onClick={() => test()}>
+      <div className="dropzone-preview py-2">
         <div>{index + 1}枚目</div>
         <div className="product-preview-image d-flex">
           <Image src={image_path} className="dropzone-image" onClick={changeImage(id)} />
@@ -214,7 +209,8 @@ export default () => {
 
     // 画像保存stateに値があればAPI発火
     if (storeProductImages.length > 0) {
-      storeProductImages.forEach((image) => formData.append("files[]", image, image.name));
+      console.log(storeProductImages);
+      // storeProductImages.forEach((image) => formData.append("files[]", image, image.name));
     }
     
     if (pathname.includes('/edit')) {

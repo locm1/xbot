09d9a64f6ae9365b776d467d6b8c7b3e1ef0a639
locm1 <ak.47_teytey@icloud.com>
2023-06-19 10:33:32 +0900

    カレンダー

--- app/Http/Requests/management/event_calendar/UpdateEventCalendarRequest.php
+++ app/Http/Requests/management/event_calendar/UpdateEventCalendarRequest.php
@@ -26,37 +26,32 @@ class UpdateEventCalendarRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => ['required', 'string', 'max:50'],
-            'start_date' => ['required', 'date'],
-            'end_date' => ['date'],
-            'location' => ['required', 'max:50'],
-            'remaining' => ['required', 'int'],
-            'is_unlimited' => ['required', 'int'],
+            'title' => 'nullable|max:50',
+            'start_time' => 'required|date_format:H:i',
+            'end_time' => 'required|date_format:H:i|after:start_time',
+            'remaining' => 'required|int',
+            // 'title' => ['required', 'string', 'max:50'],
+            // 'start_date' => ['required', 'date'],
+            // 'end_date' => ['date'],
+            // 'location' => ['required', 'max:50'],
+            // 'remaining' => ['required', 'int'],
+            // 'is_unlimited' => ['required', 'int'],
         ];
     }
 
-    /**
-     * 定義済みバリデーションルールのエラーメッセージ取得
-     *
-     * @return array
-     */
-    public function messages()
+    public function attributes()
     {
         return [
-            'title.required' => '題名は必須です',
-            'title.string' => '題名の値が不正です',
-            'title.max' => '題名は50文字までです',
-            'start_date.required' => '開始日付は必須です',
-            'end_date.after' => '終了日付が開始日付よりも前になっています',
-            'location.required' => '場所は必須です',
-            'remaining.required' => '残数は必須です',
-            'is_unlimited.required' => '値が不正です',
+            'title' => '題名',
+            'start_time' => '開始時刻',
+            'end_time' => '終了時刻',
+            'remaining' => '残数',
         ];
     }
 
     protected function failedValidation(Validator $validator)
     {
         $response['errors']  = $validator->errors()->toArray();
-        throw new HttpResponseException( response()->json( $response, 422 ));
+        throw new HttpResponseException(response()->json($response, 422));
     }
 }
